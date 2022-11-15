d51adaf48e7d4a72fd05970660d5ed5cf8895a84
Akash Shrivastava <akash.shrivastava136@gmail.com>
2022-11-15 18:11:30 +0530

    feat: Create components for portfolio

---
+++ src/components/about/About.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './about.css';
+
+const About = () => {
+  return <div>about</div>;
+};
+
+export default About;
---
+++ src/components/experience/Experience.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './experience';
+
+const Experience = () => {
+  return <div>Experience</div>;
+};
+
+export default Experience;
