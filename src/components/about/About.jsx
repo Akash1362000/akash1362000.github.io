fe7d3473b43e4587be27c2652523f6f96b7d55ef
Akash Shrivastava <akash.shrivastava136@gmail.com>
2022-11-21 08:10:47 +0530

    feat: Complete about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,54 @@
 import React from 'react';
 import './about.css';
+import Akash_Profile from '../../assets/Akash_Profile.png';
+import { SlBadge } from 'react-icons/sl';
+import { BsPeopleFill } from 'react-icons/bs';
+import { ImFolderOpen } from 'react-icons/im';
 
 const About = () => {
-  return <section id="about">About</section>;
+  return (
+    <section id="about">
+      <h5>Get to Know</h5>
+      <h2>About Me</h2>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={Akash_Profile} alt="Akash Shrivastava" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <SlBadge className="about__icon" />
+              <h5>Experience</h5>
+              <small>1+ years Working</small>
+            </article>
+            <article className="about__card">
+              <BsPeopleFill className="about__icon" />
+              <h5>Clients</h5>
+              <small>3 Clients</small>
+            </article>
+            <article className="about__card">
+              <ImFolderOpen className="about__icon" />
+              <h5>Projects</h5>
+              <small>5 projects</small>
+            </article>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos illo
+            distinctio numquam ipsum! Error, sit nobis! Incidunt nobis veritatis
+            hic mollitia dicta. Laudantium aut nemo eos magni quae qui,
+            molestias, repellat modi temporibus vel nulla tempora, voluptate
+            nobis corrupti eius laboriosam! Quidem aliquid esse minus ipsum nam
+            laborum rerum ipsa.
+          </p>
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,23 +4,46 @@ import { AiOutlineHome, AiOutlineUser } from 'react-icons/ai';
 import { BiBook } from 'react-icons/bi';
 import { RiServiceLine } from 'react-icons/ri';
 import { TbMessages } from 'react-icons/tb';
+import { useState } from 'react';
 
 const Navbar = () => {
+  const [activeNav, setActiveNav] = useState('#');
+
   return (
     <nav>
-      <a href="#" className="active">
+      <a
+        href="#"
+        onClick={() => setActiveNav('#')}
+        className={activeNav === '#' ? 'active' : ''}
+      >
         <AiOutlineHome />
       </a>
-      <a href="#about">
+      <a
+        href="#about"
+        onClick={() => setActiveNav('#about')}
+        className={activeNav === '#about' ? 'active' : ''}
+      >
         <AiOutlineUser />
       </a>
-      <a href="#experience">
+      <a
+        href="#experience"
+        onClick={() => setActiveNav('#experience')}
+        className={activeNav === '#experience' ? 'active' : ''}
+      >
         <BiBook />
       </a>
-      <a href="#services">
+      <a
+        href="#services"
+        onClick={() => setActiveNav('#services')}
+        className={activeNav === '#services' ? 'active' : ''}
+      >
         <RiServiceLine />
       </a>
-      <a href="#contact">
+      <a
+        href="#contact"
+        onClick={() => setActiveNav('#contact')}
+        className={activeNav === '#contact' ? 'active' : ''}
+      >
         <TbMessages />
       </a>
     </nav>
